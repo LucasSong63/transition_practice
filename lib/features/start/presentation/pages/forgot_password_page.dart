24234fd8fe64f740bfe94f184777929866ab195a
Lucas Song <lucassong63@gmail.com>
2024-10-04 14:09:44 +0800

    Forgot Password Page Initialze

---
+++ lib/features/start/presentation/pages/forgot_password_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:transition_practice/features/start/presentation/widgets/custom_button.dart';
+import 'package:transition_practice/features/start/presentation/widgets/custom_textformfield.dart';
+import 'package:transition_practice/features/start/presentation/widgets/spacer_box.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (BuildContext, Orientation, ScreenType) {
+        final double pixelDensity = MediaQuery.of(context).devicePixelRatio;
+        double boxWidth = 80.w.clamp(50.0 * pixelDensity, 800.0 * pixelDensity);
+        double boxHeight = 90.h.clamp(100 * pixelDensity, 700 * pixelDensity);
+        double clampedButtonHeight =
+            (boxHeight * 0.125).clamp(15 * pixelDensity, 23 * pixelDensity);
+        return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            body: Center(
+              child: SingleChildScrollView(
+                child: Container(
+                  width: boxWidth,
+                  height: boxHeight,
+                  child: Column(
+                    children: [
+                      SpacerBox(30),
+                      Text(
+                        'Enter Email Address',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                          color: Colors.black.withOpacity(0.4),
+                          wordSpacing: 0,
+                          letterSpacing: 0,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 20),
+                        width: 80.w.clamp(100.0, 1000.0),
+                        child: Text(
+                          "A password reset link will be sent to your email to reset your password. If you don't get an email within a few minutes. Please try again.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                      SpacerBox(30),
+                      SizedBox(
+                        width: 80.w,
+                        child: CustomTextFormField(
+                          labelText: 'Email Address',
+                          hintText: 'example@gmail.com',
+                          assetPath: 'assets/images/email.png',
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Email is required';
+                            }
+                            if (!value.contains('@') || !value.contains('.')) {
+                              return 'Invalid email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      SpacerBox(15),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'Back to login',
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.4),
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                      SpacerBox(30),
+                      CustomButton(
+                          width: double.infinity,
+                          height: clampedButtonHeight,
+                          text: 'Send',
+                          onTap: () {
+                            //pop up dialog with button ok, show successful sent email
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text('Password Reset'),
+                                  content: Text(
+                                      'A password reset link has been sent to your email.'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          })
+                    ],
+                  ),
+                ),
+              ),
+            ));
+      },
+    );
+  }
+}
